0dba22198369d3664cdc338fde9623b246b70e66
Aastha Baheti <aasthabaheti1@gmail.com>
2026-07-29 12:33:58 +0530

    Time: 4 ms (10.65%), Space: 10.2 MB (81.39%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+
+int evalRPN(char** tokens, int tokensSize) {
+    int stack[tokensSize];
+    int top = -1;
+
+    for (int i = 0; i < tokensSize; i++) {
+        char *t = tokens[i];
+
+        if (strcmp(t, "+") == 0) {
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a + b;
+        }
+        else if (strcmp(t, "-") == 0) {
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a - b;
+        }
+        else if (strcmp(t, "*") == 0) {
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a * b;
+        }
+        else if (strcmp(t, "/") == 0) {
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a / b;
+        }
+        else {
+            stack[++top] = atoi(t);
+        }
+    }
+
+    return stack[top];
+}
